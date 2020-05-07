9de9fa377af4ff04a21881fff80a5d25d9d4aa74
nuccch2010 <nuccch2010@163.com>
2020-05-07 14:36:24 +0800

    fix sql statement for table ddl

--- src/main/resources/sql/schema-create-mysql-1.0.0.sql
+++ src/main/resources/sql/schema-create-mysql-1.0.0.sql
@@ -3,11 +3,11 @@
 -- flush privileges;
 
 -- 账户表
-create table if not exists `account` (
-  `id`    bigint(20)   not null auto_increment,
-  `name`  varchar(255) not null default '' comment '姓名',
-  `email` varchar(30)  not null default '' comment '邮箱',
-  `ctime` timestamp    not null default current_timestamp comment '创建时间',
-  `mtime` timestamp    not null default current_timestamp comment '最后一次修改时间',
-  primary key (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `account` (
+  `id`    bigint(20) NOT NULL AUTO_INCREMENT,
+  `name`  varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '姓名',
+  `email` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `mtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后一次修改时间',
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 DEFAULT COLLATE=utf8_unicode_ci;
--- src/main/resources/sql/schema-update-mysql-1.0.0.sql
+++ src/main/resources/sql/schema-update-mysql-1.0.0.sql
@@ -1,2 +1,2 @@
-# alter table account add column `phone` varchar(13) not null default '' comment '电话' after email;
+# ALTER TABLE account ADD COLUMN `phone` varchar(13) NOT NULL DEFAULT '' COMMENT '电话' AFTER COLUMN `email`;
 select count(1) from `account`;
